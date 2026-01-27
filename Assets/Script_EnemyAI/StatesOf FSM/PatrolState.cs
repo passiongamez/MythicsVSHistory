83ff8388b71a9e8f58a8796ed7af225d5c03140c
Anjitha05 <anjithab2429@gmail.com>
2026-01-27 19:52:35 +0530

    Patrol State
    Simple Patrol state script

--- Assets/Script_EnemyAI/StatesOf FSM/PatrolState.cs
+++ Assets/Script_EnemyAI/StatesOf FSM/PatrolState.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AI;
 
 public class PatrolState : StateMachineBehaviour
 {
      private float timer;
+     List<Transform> wayPoints= new List<Transform>();
+     NavMeshAgent agent;
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        agent= animator.GetComponent<NavMeshAgent>();
         timer=0;
+        GameObject go = GameObject.FindGameObjectWithTag("WayPoints");
+        foreach(Transform t in go.transform)
+        {
+            wayPoints.Add(t);
+        }
+        agent.SetDestination(wayPoints[Random.Range(0,wayPoints.Count)].position);
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if(agent.remainingDistance<=agent.stoppingDistance)
+        {
+            agent.SetDestination(wayPoints[Random.Range(0,wayPoints.Count)].position);
+        }
        timer+=Time.deltaTime;
        if(timer>10)
         {
@@ -22,7 +37,7 @@ public class PatrolState : StateMachineBehaviour
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+       agent.SetDestination(agent.transform.position);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
